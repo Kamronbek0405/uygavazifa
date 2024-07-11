9359d8bfa25c3d6d01999f980bd56ca28708c79e
Kamran0504 <kamrannutbook05@gmail.com>
2024-07-11 14:32:39 +0500

    editSite

--- src/components/section2/section2.jsx
+++ src/components/section2/section2.jsx
@@ -1,5 +1,5 @@
 export const Section2 = () => {
-    return <div className="section-title w-[100%] h-[100vh]  flex items-center justify-center">
+    return <div className="section-title w-[100%] h-[50vh]  flex items-center justify-center">
         <div className="drction__container w-[90%] h-auto  flex items-center justify-between flex-col">
             <ul className="section__item flex gap-10  ">
                 <li className=" section__list w-[103px] h-[49px] bg-slate-200 flex items-center justify-center">
@@ -12,9 +12,7 @@ export const Section2 = () => {
                     <a href="">Новинки</a>
                     </li>
             </ul>
-            <div className="w-[80%] h-[492px] bg-yellow-500">
-
-            </div>
+           
         </div>
     </div>
 }
